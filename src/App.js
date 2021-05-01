c8d65f66ecd8cca4eff908d0d3dcd4ad2153e30b
Manuel Barral <manuba9017@gmail.com>
2021-05-01 00:48:26 -0300

    added new components, others changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import {React, Fragment} from 'react';
+import {React, useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Title from './components/Title/Title';
 import Menu from './components/Menu/Menu';
 import SectionInfo from './components/SectionInfo/SectionInfo';
 import Footer from './components/Footer/Footer';
 import Whattsapp from './components/Whattsapp/Whattsapp';
+import {Roller} from 'react-spinners-css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import classes from './App.module.css';
 
 const App = () => {
+	const [loading, setLoading] = useState(true);
+
+	setTimeout(() => {
+		setLoading(false);
+	}, 1000);
+
 	return (
-		<Fragment>
-			<div className={classes.root}>
-				<Grid container className={classes.container}>
-					<Grid item xs={12}>
-						<Title />
-						<Menu />
+		<div className={classes.app}>
+			{loading ? (
+				<Roller
+					load={loading}
+					className={classes.spinner}
+					color='#67B426'
+				/>
+			) : (
+				<div className={classes.root}>
+					<Grid container className={classes.container}>
+						<Grid item xs={12}>
+							<Title />
+							<Menu />
+						</Grid>
 					</Grid>
-				</Grid>
-				<Grid item xs={12}>
-					<SectionInfo />
-					<Whattsapp />
-					<Footer />
-				</Grid>
-			</div>
-		</Fragment>
+					<Grid item xs={12}>
+						<SectionInfo />
+						<Whattsapp />
+						<Footer />
+					</Grid>
+				</div>
+			)}
+		</div>
 	);
 };
 
--- src/components/CarouselImage/CarouselImage.js
+++ src/components/CarouselImage/CarouselImage.js
@@ -1,58 +1,69 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+
+import classes from './CarouselImage.module.css';
 
 import img_1 from '../../img/medicina_1.png';
 import img_2 from '../../img/medicina_2.png';
 import img_3 from '../../img/medicina_3.png';
 
-import classes from './CarouselImage.module.css';
-
 const images = [
 	{
+		title: 'CLÍNICA PUEYRREDÓN',
 		image: img_1,
-		title: 'Medicina foto uno',
 	},
 	{
+		title: 'CLÍNICA 25 DE MAYO',
 		image: img_2,
-		title: 'Medicina foto dos',
 	},
 	{
+		title: 'NUESTROS PROFESIONALES',
 		image: img_3,
-		title: 'Medicina foto tres',
 	},
 ];
 
 const CarouselImage = () => {
+	const [index, setIndex] = useState(0);
+
+	const handleSelect = (selectedIndex, e) => {
+		setIndex(selectedIndex);
+	};
+
 	return (
-		<Carousel fade className={classes.carousel}>
+		<Carousel
+			activeIndex={index}
+			onSelect={handleSelect}
+			className={classes.carousel}>
 			<Carousel.Item>
 				<img
-					className={classes.img}
+					className='d-block w-100'
 					src={images[0].image}
 					alt={images[0].title}
 				/>
 				<Carousel.Caption className={classes.caption}>
-					<h2 className={classes.title}>Clínica Pueyrredón</h2>
+					<h2 className={classes.title}>{images[0].title}</h2>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img
-					className={classes.img}
+					className='d-block w-100'
 					src={images[1].image}
 					alt={images[1].title}
 				/>
+
 				<Carousel.Caption className={classes.caption}>
-					<h2 className={classes.title}>Clínica 25 de Mayo</h2>
+					<h2 className={classes.title}>{images[1].title}</h2>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img
-					className={classes.img}
+					className='d-block w-100'
 					src={images[2].image}
 					alt={images[2].title}
 				/>
+
 				<Carousel.Caption className={classes.caption}>
-					<h2 className={classes.title}>Nuestros profesionales</h2>
+					<h2 className={classes.title}>{images[2].title}</h2>
 				</Carousel.Caption>
 			</Carousel.Item>
 		</Carousel>
